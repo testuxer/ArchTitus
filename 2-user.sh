d674fbde9a9c1ee0eb2313df25f7fccf92f68e1f
testuxer <93405616+testuxer@users.noreply.github.com>
2021-10-29 22:25:51 +0200

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -25,13 +25,11 @@ ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
 PKGS=(
 'autojump'
 'awesome-terminal-fonts'
-'brave-bin' # Brave Browser
 'dxvk-bin' # DXVK DirectX to Vulcan
-'github-desktop-bin' # Github Desktop sync
-'lightly-git'
 'mangohud' # Gaming FPS Counter
 'mangohud-common'
 'nerd-fonts-fira-code'
+'noisetorch'
 'nordic-darker-standard-buttons-theme'
 'nordic-darker-theme'
 'nordic-kde-git'
@@ -39,6 +37,7 @@ PKGS=(
 'noto-fonts-emoji'
 'papirus-icon-theme'
 'plasma-pa'
+'protonvpn-cli-ng'
 'ocs-url' # install packages from websites
 'sddm-nordic-theme-git'
 'snapper-gui-git'
@@ -46,7 +45,6 @@ PKGS=(
 'ttf-hack'
 'ttf-meslo' # Nerdfont package
 'ttf-roboto'
-'zoom' # video conferences
 'snap-pac'
 )
 
